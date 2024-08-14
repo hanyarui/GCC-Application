72718de464e780974be9a23c1d76a9de9bd1f3cd
Dharu Aulia Rahman <106579507+hanyarui@users.noreply.github.com>
2024-08-14 16:10:35 +0700

    Update

--- app/src/main/java/com/gcc/gccapplication/data/local/UserPreferences.kt
+++ app/src/main/java/com/gcc/gccapplication/data/local/UserPreferences.kt
@@ -2,9 +2,6 @@ package com.gcc.gccapplication.data.local
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.util.Log
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 
 class UserPreferences(context: Context) {
 
@@ -12,11 +9,22 @@ class UserPreferences(context: Context) {
 
     companion object {
         private const val PREFS_NAME = "userPreferences"
+        private const val TOKEN_KEY = "token"
         private const val EMAIL_KEY = "email"
         private const val NAME_KEY = "fullName"
         private const val ROLE_KEY = "role"  // Tambahkan konstanta untuk role
         private const val ADDRESS_KEY = "address"
-        private const val UID_KEY = "uid"
+    }
+
+    fun saveToken(token: String) {
+        with(prefs.edit()) {
+            putString(TOKEN_KEY, token)
+            apply()
+        }
+    }
+
+    fun getToken(): String? {
+        return prefs.getString(TOKEN_KEY, null)
     }
 
     fun saveEmail(email: String) {
@@ -49,14 +57,6 @@ class UserPreferences(context: Context) {
         }
     }
 
-    fun firebaseCurrrentUser(): FirebaseUser? {
-        return FirebaseAuth.getInstance().currentUser
-    }
-
-    fun firebaseSignOut(){
-        FirebaseAuth.getInstance().signOut()
-    }
-
     fun getRole(): String? {
         return prefs.getString(ROLE_KEY, null)
     }
@@ -77,34 +77,5 @@ class UserPreferences(context: Context) {
             clear()
             apply()
         }
-        // Debugging line to check if preferences are actually cleared
-        Log.d("Preferences", "All preferences cleared: ${prefs.all}")
-    }
-
-
-    fun getUid(): String? {
-        return prefs.getString(UID_KEY, null)
-    }
-    // Fungsi untuk mendapatkan semua data user dalam satu objek User
-    fun getUserData(): User? {
-        val email = getEmail()
-        val fullName = getFullName()
-        val role = getRole()
-        val address = getAddress()
-        val uid = getUid().toString()
-
-        return if (email != null && fullName != null && role != null && address != null) {
-            User(email, fullName, role, address,uid)
-        } else {
-            null
-        }
     }
 }
-
-data class User(
-    val email: String,
-    val fullName: String,
-    val role: String,
-    val address: String,
-    val Uid: String
-)
--- app/src/main/java/com/gcc/gccapplication/ui/activity/MainActivity.kt
+++ app/src/main/java/com/gcc/gccapplication/ui/activity/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.gcc.gccapplication.ui.activity
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.gcc.gccapplication.R
 import com.gcc.gccapplication.databinding.ActivityMainBinding
+import android.content.Context
 import android.content.Intent
 import android.os.Handler
 import android.os.Looper
-import com.gcc.gccapplication.data.local.UserPreferences
-//import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var userPreferences: UserPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,25 +22,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        userPreferences = UserPreferences(this)
-
         // Menunda eksekusi untuk menunjukkan splash screen
         Handler(Looper.getMainLooper()).postDelayed({
-            checkUserAndNavigate()
-        }, 3000) // 3000 ms (3 detik) adalah durasi splash screen
+            checkTokenAndNavigate()
+        }, 3000) // 2000 ms (2 detik) adalah durasi splash screen
+
     }
 
-    private fun checkUserAndNavigate() {
-        val currentUser = userPreferences.firebaseCurrrentUser()
+    private fun checkTokenAndNavigate() {
+        // Mengambil SharedPreferences
+        val sharedPref = getSharedPreferences("userPreferences", Context.MODE_PRIVATE)
+        val token = sharedPref.getString("token", null)
 
-        if (currentUser != null) {
-//            currentUser != null
-            // Jika pengguna sudah login, arahkan ke PageActivity
+        if (token != null) {
+            // Jika token ditemukan, arahkan ke PageActivity
             val intent = Intent(this, PageActivity::class.java)
             startActivity(intent)
             finish()
         } else {
-            // Jika pengguna belum login, arahkan ke ValidationActivity
+            // Jika token tidak ditemukan, arahkan ke ValidationPage
             val intent = Intent(this, ValidationActivity::class.java)
             startActivity(intent)
             finish()
